9d77a4f1fdc069812587c9f0129797ff5455fb92
Welington Cordeiro <welington.batista@basis.com.br>
2019-02-12 14:29:33 -0200

    Configuração angular

---
+++ money-ui/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {TabViewModule} from 'primeng/tabview';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TabViewModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
