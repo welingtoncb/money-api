c0b55afc31716d3d514fdb1e0ea3cbc1d8740b4d
Welington Cordeiro <welington.batista@basis.com.br>
2019-02-13 14:11:24 -0200

    Implementação - Páginas de pesquisas

--- money-ui/src/app/app.module.ts
+++ money-ui/src/app/app.module.ts
@@ -1,17 +1,27 @@
-import { TabViewModule } from 'primeng/tabview';
-// import { TabViewModule } from 'primeng/primeng';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { InputTextModule } from 'primeng/components/inputtext/inputtext'
+import { ButtonModule } from 'primeng/components/button/button'
+import { DataTableModule } from 'primeng/components/datatable/datatable'
+import { TooltipModule } from 'primeng/components/tooltip/tooltip'
+
 import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    TabViewModule
+    InputTextModule,
+    ButtonModule,
+    DataTableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
