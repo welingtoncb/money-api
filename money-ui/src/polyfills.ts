6e02fe8f5c13e621400a157a45937dc98502e31e
Welington Cordeiro <welingtoncb@yahoo.com.br>
2019-05-03 13:53:28 -0300

    Commit importante dia 03/05

--- money-ui/src/polyfills.ts
+++ money-ui/src/polyfills.ts
@@ -1,3 +1,5 @@
+import 'zone.js';
+import 'reflect-metadata';
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -43,7 +45,6 @@
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
 
 
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
