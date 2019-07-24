78172f6cb66351129765293b39f1c61b397824d6
Welington Cordeiro <welingtoncb@yahoo.com.br>
2019-07-24 14:45:41 -0300

    Implementação de recursos para refresh token expirado

--- money-ui/src/app/core/error-handler.service.ts
+++ money-ui/src/app/core/error-handler.service.ts
@@ -2,17 +2,26 @@ import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
 
 import { ToastyService } from 'ng2-toasty';
+import { Router } from '@angular/router';
+import { NotAuthenticatedError } from 'app/seguranca/money-http';
 
 @Injectable()
 export class ErrorHandlerService {
 
-  constructor(private toasty: ToastyService) { }
+  constructor(
+    private toasty: ToastyService,
+    private router: Router
+  ) { }
 
   handle(errorResponse: any) {
     let msg: string;
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      msg = 'Sua sessão expirou';
+      this.router.navigate(['/login']);
+
     } else if (errorResponse instanceof Response && errorResponse.status >= 400 && errorResponse.status <= 499) {
       let errors: any;
 
