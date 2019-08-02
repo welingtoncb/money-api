6f3706d693392c9b7519eec8d14f0e2bef2d0d73
Welington Cordeiro <welingtoncb@yahoo.com.br>
2019-08-02 13:55:21 -0300

    Correção do locale

--- money-ui/src/app/core/core.module.ts
+++ money-ui/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { Title } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { ConfirmationService } from 'primeng/components/common/api';
 import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
@@ -16,6 +17,8 @@ import { RouterModule } from '@angular/router';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada.component';
 import { AuthService } from 'app/seguranca/auth.service';
 import { NaoAutorizadoComponent } from './nao-autorizado.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   imports: [
@@ -45,7 +48,7 @@ import { NaoAutorizadoComponent } from './nao-autorizado.component';
     ConfirmationService,
     JwtHelper,
     Title,
-    { provide: LOCALE_ID, useValue: 'pt-BR' }
+    { provide: LOCALE_ID, useValue: 'pt' }
   ]
 })
 export class CoreModule { }
